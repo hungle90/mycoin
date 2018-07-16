f70a8e1501e7c0a918f0a4451df5fe6a55431920
hungle90 <lehung229@gmail.com>
2018-07-16 15:05:53 +0700

    Update Test

--- test/contract_test.js
+++ test/contract_test.js
@@ -7,9 +7,8 @@ contract('contractTestDemo', function(accounts) {
     var contract_test = contractTestDemo.deployed();
     assert.isTrue(true);
     done();
-  });
+    });
 });
-
 contract('Ownable', function (accounts) {
   it("owner address is the first account", function () {
     var meta;
@@ -22,34 +21,3 @@ contract('Ownable', function (accounts) {
       });
     });
   });
-
-contract('StandardToken', function (accounts) {
-  it("should send coin correctly", async () => {
-
-    // Get initial balances of first and second account.
-      let account_one = accounts[0];
-      let account_two = accounts[1];
-
-      let amount = 10;
-
-
-      let instance = await StandardToken.deployed();
-      let meta = instance;
-
-      let balance = await meta.balanceOf(account_one).call({from: account_one});
-      let account_one_starting_balance = balance.toNumber();
-
-      balance = await meta.balanceOf(account_two).call({from: account_two});
-      let account_two_starting_balance = balance.toNumber();
-      await meta.transferFrom(account_one_starting_balance, account_two_ending_balance, amount);
-
-      balance = await meta.balanceOf(account_one);
-      let account_one_ending_balance = balance.toNumber();
-
-      balance = await meta.balanceOf.call(account_two);
-      let account_two_ending_balance = balance.toNumber();
-
-      assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount wasn't correctly taken from the sender");
-      assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
-      });
-});
